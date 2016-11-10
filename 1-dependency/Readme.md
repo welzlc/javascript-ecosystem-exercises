36d0a54cbe7ccd576910a915194bea3c4e9c9324
chris <chris@chriss-MacBook.local>
2016-11-10 14:16:18 +0100

    rollup part implemented

--- 1-dependency/Readme.md
+++ 1-dependency/Readme.md
@@ -1,5 +1,23 @@
 # Lesson: Dependeny Management
 ## Todo
+1. Start todo app (use a static webserver) and try it out
 1. Init a package.json
 1. Install bootstrap as dependency
 1. Link bootstrap.min.css in index.html
+
+## Static Webserver
+### MacOs
+```
+ python -m SimpleHTTPServer
+```
+
+### Windows / MacOs / Linux
+Install
+```
+ npm install http-server -g
+```
+
+Run
+```
+http-server
+```
---
+++ 3-modularization-webpack/src/todo-store.js
@@ -0,0 +1,64 @@
+import { statusComparator } from './status-comparator';
+
+/**
+ * TodoStore handles and stores all todos
+ */
+export class TodoStore {
+
+    constructor() {
+        this.idCounter = 0;
+        this._todos = [];
+        this.subscribers = [];
+    }
+
+    subscribe(subscriber) {
+        this.subscribers.push(subscriber);
+        this.notify();
+    }
+
+    notify() {
+        this.subscribers.forEach(subscriber => {
+            subscriber.call(subscriber, this.todos);
+        });
+    }
+
+    getAndIncrementIdCounter() {
+        return this.idCounter++;
+    }
+
+    addTodo(description) {
+        const todoObject = {
+            timeStamp: new Date(),
+            description: description,
+            id: this.getAndIncrementIdCounter(),
+            done: false
+        };
+        this._todos.push(todoObject);
+        this.notify();
+    }
+
+    setDone(id) {
+        const matchingTodos = this._todos.filter(function(todo){
+            return todo.id === id;
+        });
+
+        if (matchingTodos.length === 1) {
+            matchingTodos[0].done = true;
+            this._todos.sort(statusComparator);
+        } else {
+            throw new Error(`No Todo item with id ${id} found!`);
+        }
+
+        this.notify();
+    }
+
+    get todos() {
+        return this._todos;
+    }
+
+    clear() {
+        this.idCounter = 0;
+        this._todos = [];
+        this.notify();
+    }
+}
