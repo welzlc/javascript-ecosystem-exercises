7923b8a772f0aafcc402463c564495aae1f2fee2
Nick Payne <nick.payne89@gmail.com>
2018-04-12 13:56:31 +0200

    lint

--- 6-testing/webpack.config.js
+++ 6-testing/webpack.config.js
@@ -1,21 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: './src/app.js',
-  output: {
-    filename: 'bundle.js',
-    path: './dist'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader?presets[]=es2015' },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/, loader: "file-loader" }
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: './dist'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader?presets[]=es2015' },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/, loader: 'file-loader' }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './index.html' })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({template: './index.html'})
-  ]
 };
 
 module.exports = config;
